339157245191ee1eff47cfb9b5e3a9455989e983
Ankit Shaw <shawankit.as@gmail.com>
2025-12-28 18:26:20 +0530

    Vite react Basics

---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import React from 'react'
+
+/* function MyApp(){
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+} */
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+);
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',target: '_blank'},
+  'click me to visit google'
+)
+createRoot(document.getElementById('root')).render(
+  //<reactElement/>
+  //<MyApp/>
+  //<anotherElement/>
+  <App/>
+)
